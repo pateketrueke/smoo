323325c110e6d4f9420961bc19f687a24214d476
Alvaro Cabrera Durán <pateketrueke@gmail.com>
2021-05-25 00:13:27 -0500

    feat: quick Failure support, cleanup

--- src/main.js
+++ src/main.js
@@ -1,2 +1,3 @@
 export { default as Fence } from './components/Fence.svelte';
 export { default as Search } from './components/Search.svelte';
+export { default as Failure } from './components/Failure.svelte';
